a4c6d17480540acb3c055b4729335939f4b5f4e3
HyphaSlayer <tiebo.croons@gmail.com>
2025-02-23 14:24:44 +0100

    Feat: Implement navigation between Home and ProductDetails screens

--- App.js
+++ App.js
@@ -1,48 +1,21 @@
-import React from 'react';
-import { View, ScrollView, StyleSheet, StatusBar } from 'react-native';
-import ProductCard from './components/ProductCart';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home';
+import ProductDetails from './screens/ProductDetails';
 
-const products = [
-  {
-    name: 'Product 1',
-    description: 'This is the description for product 1.',
-    image: 'https://picsum.photos/100',
-  },
-  {
-    name: 'Product 2',
-    description: 'This is the description for product 2.',
-    image: 'https://picsum.photos/100',
-  },
-  {
-    name: 'Product 3',
-    description: 'This is the description for product 3.',
-    image: 'https://picsum.photos/100',
-  },
-];
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scrollViewContent}>
-        {products.map((product, index) => (
-          <ProductCard key={index} product={product} />
-        ))}
-      </ScrollView>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ProductDetails" component={ProductDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f0f0f0',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  scrollViewContent: {
-    paddingTop: 150, // Add padding to the top of the ScrollView content
-  },
-});
 
 export default App;
