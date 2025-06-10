86505206bccbd962d43733af94b89dd2bbc197db
HyphaSlayer <tiebo.croons@gmail.com>
2025-06-10 20:41:54 +0200

    feat: Add dotenv plugin configuration for environment variable management

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'], // Use only the required preset
   plugins: [
+    ['module:react-native-dotenv', {
+      moduleName: '@env',
+      path: '.env',
+      safe: false,
+      allowUndefined: true,
+    }],
     ['@babel/plugin-transform-private-methods', { loose: true }], // Add this plugin
     'react-native-reanimated/plugin', // Ensure this is the last plugin if you're using react-native-reanimated
   ],
