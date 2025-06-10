c806ad8e36d2a66251fafb30f442439899e3f101
HyphaSlayer <tiebo.croons@gmail.com>
2025-06-10 21:11:39 +0200

    refactor: Improve comments in index.js for better clarity and understanding

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import { registerRootComponent } from 'expo';
+// Importeer de functie `registerRootComponent` van Expo.
 
 import App from './App';
+// Importeer de hoofdcomponent van de app.
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+
+// `registerRootComponent` roept `AppRegistry.registerComponent('main', () => App)` aan.
+// Het zorgt er ook voor dat, ongeacht of je de app laadt in Expo Go of in een native build,
+// de omgeving correct wordt ingesteld.
 registerRootComponent(App);
+// Registreer de hoofdcomponent van de app zodat deze correct wordt geladen.
